0c6770a76178d2c44d642c407a00f22316447eb6
Nicholas Rushton <nick.rushton@hotmail.com>
2013-09-02 17:08:49 -0700

    adding new controller for interview * adding bower dependencies * adding startup template

--- public/app/scripts/controllers/interview.js
+++ public/app/scripts/controllers/interview.js
@@ -1,22 +1,10 @@
 'use strict';
 
 angular.module('publicApp')
-  .controller('InterviewCtrl', ['socket', function ($scope, socket) {
-    $scope.modes = ['Scheme', 'XML', 'Javascript', 'Python'];
-    $scope.mode = $scope.modes[0];
-
-    // The ui-ace option
-    $scope.aceOption = {
-      mode: $scope.mode.toLowerCase(),
-      onLoad: function (_ace) {
-        $scope.modeChanged = function () {
-          _ace.getSession().setMode("ace/mode/" + $scope.mode.toLowerCase());
-        };
-
-      }
-    };
-
-    $scope.on('socket:error', function(eve, data) {
-        socket.emit('text-changed', {id: "hello" , value:'world'});
-    });
-  }]);
+  .controller('InterviewCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
