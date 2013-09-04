b95843fcde50a69068fc7d1549eec6c4e4802351
Jay Zeng <jayzeng@jay-zeng.com>
2013-09-03 23:27:40 -0700

    * Pull info from Github

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('publicApp', ['fireace'])
+angular.module('publicApp', ['fireace', 'ngResource'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- public/app/scripts/controllers/interview.js
+++ public/app/scripts/controllers/interview.js
@@ -1,16 +1,26 @@
 'use strict';
 
 angular.module('publicApp')
-  .controller('InterviewCtrl', function ($scope, $routeParams) {
-     $scope.questions = [{id: 1, question:"how old are you?"}]
-     $scope.techQuestions = [{id: 2,question: "Write a function to square a number"},{id: 3, question: "write a function to be magic!"}];
-     $scope.hash = $routeParams.hash
-     $scope.candidateSummary = {
-        name: 'Carter Rabasa',
-        organizations: [{name:'CascadiaJS'},{name:'Twilio'}],
-        proficientLanguage: 'JavaScript',
-        notableProjects: [{name:'JQuery', description: 'Front-end JavaScript framework'},
-                          {name: 'CoffeeScript', description: 'Dialect of JavaScript'}],
-        imageUrl: 'https://1.gravatar.com/avatar/6417ca1c133ad9c74479a0f9a3f0dc95?d=https%3A%2F%2Fidenticons.github.com%2F261209d3a391e1b25377435d6cb41382.png&s=200'
-     };
-  });
+  .controller('InterviewCtrl', ['$scope', '$routeParams', 'interviewQuestion', 'github',
+     function ($scope, $routeParams, interviewQuestion, github) {
+         $scope.questions = [{id: 1, question:"how old are you?"}]
+
+         $scope.techQuestions = [{id: 2,question: "Write a function to square a number"},{id: 3, question: "write a function to be magic!"}];
+         $scope.hash = $routeParams.hash
+
+         var userParam = 'crabasa';
+
+         github.get({
+           user: userParam,
+           repo: ''
+         }, function(res) {
+           $scope.summary = res.data;
+         });
+
+         $scope.candidateSummary = {
+            organizations: [{name:'CascadiaJS'},{name:'Twilio'}],
+            proficientLanguage: 'JavaScript',
+            notableProjects: [{name:'JQuery', description: 'Front-end JavaScript framework'},
+                              {name: 'CoffeeScript', description: 'Dialect of JavaScript'}],
+         };
+  }]);
