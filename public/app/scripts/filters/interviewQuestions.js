ee53e442e52b80c1a2f8091d591c744154071657
Jay Zeng <jayzeng@jay-zeng.com>
2013-09-08 17:49:17 -0700

    * Ensure field exists * Purge unused file

--- public/app/scripts/filters/interviewQuestions.js
+++ public/app/scripts/filters/interviewQuestions.js
@@ -13,10 +13,22 @@ angular.module('publicApp')
          searchString = searchString.toLowerCase();
 
          angular.forEach(arr, function(item) {
-              if(item.title.toLowerCase().indexOf(searchString) !== -1 ||
-                 item.body.toLowerCase().indexOf(searchString) !== -1 ||
-                 item.created.toLowerCase().indexOf(searchString) !== -1 ) {
-                   result.push(item);
+              if(item.hasOwnProperty('title') && item.title) {
+                if(item.title.toLowerCase().indexOf(searchString) !== -1) {
+                    result.push(item);
+                }
+              }
+
+              if(item.hasOwnProperty('created') && item.created) {
+                if(item.created.toLowerCase().indexOf(searchString) !== -1) {
+                    result.push(item);
+                }
+              }
+
+              if(item.hasOwnProperty('body') && item.body) {
+                if(item.body.toLowerCase().indexOf(searchString) !== -1) {
+                    result.push(item);
+                }
               }
 
               if(item.hasOwnProperty('source') && item.source) {
